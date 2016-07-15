89e759aab6866da0f8ef7469e996ba10ca208e17
super <super@HyeonWoo>
2016-07-15 19:33:26 +0900

    Controller todtjd

--- src/main/java/com/javalec/myboard_controller/BController.java
+++ src/main/java/com/javalec/myboard_controller/BController.java
@@ -1,9 +1,25 @@
 package com.javalec.myboard_controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.javalec.myboard_command.BCommand;
+import com.javalec.myboard_command.BListCommand;
 
 @Controller
 public class BController {
-
+	
+	BCommand command;
+	
+	@RequestMapping("/list")
+	public String list(Model model){
+		
+		command = new BListCommand();
+		command.execute(model);
+		
+		return "list";
+	}
+	
 
 }
